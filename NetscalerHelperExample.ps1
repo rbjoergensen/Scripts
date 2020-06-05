dd0f5a56225058025268f7ec029404c812bb0985
rbjoergensen <rbjoergensen@users.noreply.github.com>
2020-06-05 18:05:15 +0200

    Create NetscalerHelperExample.ps1

---
+++ NetscalerHelperExample.ps1
@@ -0,0 +1,19 @@
+# Create variables once based on application and environment
+$Endpoint = "https://lb-lan.cotv.net/nitro/v1"
+$AppName  = "MyApp"
+$AppEnv   = "Staging"
+$FrontIp  = "10.122.4.100"
+$LBVS     = "LBVS_"+$AppName+"_"+$AppEnv+"_HTTPS"
+$SG       = "SG_"+$AppName+"_"+$AppEnv+"_HTTPS"
+$MON      = "MON_"+$AppName+"_"+$AppEnv+"_HTTPS_"
+
+. "\\cotv.net\files\Scripts\Helpers\NetscalerApiHelper.ps1"
+
+# Print the getting started help message
+Help
+
+# Start a session with the endpoint
+Login
+
+# Exit the session
+Logout
