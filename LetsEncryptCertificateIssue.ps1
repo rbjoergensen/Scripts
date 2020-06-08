e8665f3338c8ced4e25d9439b619fc3c897b5b76
Rasmus Bo Jørgensen <rbj@sampension.dk>
2020-06-08 11:37:19 +0200

    Added module installation/loading

--- LetsEncryptCertificateIssue.ps1
+++ LetsEncryptCertificateIssue.ps1
@@ -1,6 +1,8 @@
 # Based on https://github.com/rmbolger/Posh-ACME/blob/master/Tutorial.md
 
-# Variables
+###################################################################
+### Variables #####################################################
+###################################################################
 $r53Secret         = ConvertTo-SecureString -String "MyAwsSecretKey" -AsPlainText -Force
 $r53Params         = @{R53AccessKey="MyAwsAccountKey"; R53SecretKey=$r53Secret}
 $pfxPass           = "MyPfxPassword"
@@ -11,6 +13,38 @@ $contact           = "devops@cotv.dk"
 $outputDir         = "C:\Users\Solenya\Desktop\Vault"
 $zipFile           = "$outputDir\CertificateFiles.zip"
 
+###################################################################
+### Load/Install Modules ##########################################
+###################################################################
+function Load-Module ($m) {
+        # If module is imported say that and do nothing
+        if (Get-Module | Where-Object {$_.Name -eq $m}) {
+            write-host "Module $m is already imported."
+        }
+        else {
+            # If module is not imported, but available on disk then import
+            if (Get-Module -ListAvailable | Where-Object {$_.Name -eq $m}) {
+                Import-Module $m -Verbose
+            }
+            else {
+                # If module is not imported, not available on disk, but is in online gallery then install and import
+                if (Find-Module -Name $m | Where-Object {$_.Name -eq $m}) {
+                    Install-Module -Name $m -Force -Verbose -Scope CurrentUser
+                    Import-Module $m -Verbose
+                }
+                else {
+                    # If module is not imported, not available and not in online gallery then abort
+                    write-host "Module $m not imported, not available and not in online gallery, exiting."
+                    EXIT 1
+                }
+            }
+        }
+    }
+    Load-Module "Posh-ACME"
+
+###################################################################
+### Issue and save Certificate ####################################
+###################################################################
 New-PACertificate $domainNames -AcceptTOS -Contact $contact -DnsPlugin Route53 -PluginArgs $r53Params -PfxPass $pfxPass -FriendlyName $friendlyName -DirectoryUrl $letsEncryptServer -Verbose
 # Move the files from the AppData folder
 $certFolder = (Get-Item -Path (Get-PACertificate | Select -ExpandProperty CertFile)).DirectoryName
